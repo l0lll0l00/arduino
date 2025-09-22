2daaf714dc9a122ef74babaec9d62f4d8abe25ca
l0lll0l00 <leeyoujin621@gmail.com>
2025-09-22 21:41:30 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,23 @@
+const int ledPin = 7;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, 0);
+  delay(1000);
+
+  digitalWrite(ledPin, 1);
+  delay(200);
+
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(ledPin, 0);
+    delay(100);
+    digitalWrite(ledPin, 1);
+    delay(100);
+  }
+
+  digitalWrite(ledPin, 1);
+  while (1) {}
+}
