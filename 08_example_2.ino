8d5522451a9135f762e9888124c5819cb0a1e54c
l0lll0l00 <leeyoujin621@gmail.com>
2025-10-02 19:46:59 +0900

    Add files via upload

---
+++ 08_example_2.ino
@@ -0,0 +1,65 @@
+#define PIN_LED  9
+#define PIN_TRIG 12
+#define PIN_ECHO 13
+
+#define SND_VEL 346.0
+#define INTERVAL 25
+#define PULSE_DURATION 10
+#define _DIST_MIN 100.0
+#define _DIST_CENTER 200.0
+#define _DIST_MAX 300.0
+
+#define TIMEOUT ((INTERVAL / 2) * 1000.0)
+#define SCALE (0.001 * 0.5 * SND_VEL)
+
+unsigned long last_sampling_time;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_TRIG, OUTPUT);  
+  pinMode(PIN_ECHO, INPUT);   
+  digitalWrite(PIN_TRIG, LOW);  
+  
+  Serial.begin(57600);
+}
+
+void loop() { 
+  float distance;
+
+  if (millis() < (last_sampling_time + INTERVAL))
+    return;
+
+  distance = USS_measure(PIN_TRIG, PIN_ECHO);
+
+  int duty = 255;
+
+  if ((distance == 0.0) || (distance < _DIST_MIN) || (distance > _DIST_MAX)) {
+    duty = 255;
+  } else {
+    if (distance <= _DIST_CENTER) {
+      duty = mapFloat(distance, _DIST_MIN, _DIST_CENTER, 255, 0);
+    } else {
+      duty = mapFloat(distance, _DIST_CENTER, _DIST_MAX, 0, 255);
+    }
+  }
+
+  analogWrite(PIN_LED, duty);
+
+  Serial.print("Distance(mm): "); Serial.print(distance);
+  Serial.print("  Duty: "); Serial.println(duty);
+
+  last_sampling_time += INTERVAL;
+}
+
+float USS_measure(int TRIG, int ECHO)
+{
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(PULSE_DURATION);
+  digitalWrite(TRIG, LOW);
+  
+  return pulseIn(ECHO, HIGH, TIMEOUT) * SCALE;
+}
+
+float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
